4703d6ef551b81ea1383dc019d8b8a11d603a27b
AshishKerikere <93969829+AshishKerikere@users.noreply.github.com>
2022-12-02 16:29:36 +0530

    User Registration UC1

--- UserRegistrationSystem.java
+++ UserRegistrationSystem.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class UserRegistrationSystem {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String userFirstName = userFirstNameInputandValidation();
+        System.out.println(userFirstName +" is a valid First User Name.");
+    }
+
+    public static String userFirstNameInputandValidation() {
+        System.out.println("Enter User First Name");
+        String userFirstName = scanner.next();
+        Pattern pattern = Pattern.compile("^[A-Z][a-z]{3,}");
+        Matcher matcher = pattern.matcher(userFirstName);
+        if (matcher.find()) {
+            return userFirstName;
+        } else {
+            userFirstName = userFirstNameInputandValidation();
+            return userFirstName;
+        }
+    }
+
+}
