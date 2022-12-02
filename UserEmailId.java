8c54b3c6561c7faca7d03466a2f1caef6ce80396
AshishKerikere <93969829+AshishKerikere@users.noreply.github.com>
2022-12-02 16:39:58 +0530

    User Registration System UC3

--- UserEmailId.java
+++ UserEmailId.java
@@ -1,1 +1,41 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class UserRegistrationSystem {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        String emailAddress = UserEmailId.evaluateTypeAndCheck();
+        System.out.println("This is correct email" +emailAddress);
+    }
+
+    public static String userFirstNameInputandValidation() {
+        System.out.println("Enter User First Name");
+        String userFirstName = scanner.next();
+        Pattern pattern = Pattern.compile("^[A-Z][a-z]{3,}");
+        Matcher matcher = pattern.matcher(userFirstName);
+        if (matcher.find()) {
+            return userFirstName;
+        } else {
+            userFirstName = userFirstNameInputandValidation();
+            return userFirstName;
+        }
+    }
+
+    public static String userLastNameInputandValidation() {
+        System.out.println("Enter User Last Name");
+        String userLastName = scanner.next();
+        Pattern pattern = Pattern.compile("^[A-Z][a-z]{3,}");
+        Matcher matcher = pattern.matcher(userLastName);
+        if(matcher.find()){
+            return userLastName;
+        }
+        else {
+            userLastName = userLastNameInputandValidation();
+            return userLastName;
+        }
+    }
+
+}
